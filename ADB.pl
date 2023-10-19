a1a8fe85158fe93fdcb166b9a9fff2839bcbdde1
sreelatha5432 <113494171+sreelatha5432@users.noreply.github.com>
2023-10-19 10:08:03 +0530

    Add files via upload

---
+++ ADB.pl
@@ -0,0 +1,8 @@
+%Database of individuals with name and dataof birth (DOB)
+%syntax: person(Name, DOB).
+
+person(john, '1990-09-06').
+person(sreelu, '1990-04-03').
+person(madhu, '1960-08-05').
+person(yamuna, '2000-03-02').
+person(bindu, '2001-09-04').
---
+++ incorporate.pl
@@ -0,0 +1,6 @@
+bird(crow).
+bird(sparrow).
+bird(penguin).
+
+can_fly(crow).
+can_fly(sparrow).
